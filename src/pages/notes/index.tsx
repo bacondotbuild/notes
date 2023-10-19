1c9c10029911b50d0a75780e4ba41c7c6dc7375a
bacon.build <bacondotbuild@gmail.com>
2023-10-18 17:00:23 -0700

    add filter to notes page

--- src/pages/notes/index.tsx
+++ src/pages/notes/index.tsx
@@ -9,13 +9,33 @@ import { api } from '@/lib/api'
 import {
   ArrowLeftOnRectangleIcon,
   ArrowRightOnRectangleIcon,
+  UserGroupIcon,
+  UserIcon,
+  XMarkIcon,
 } from '@heroicons/react/24/solid'
 import Button from '@/components/design/button'
+import Footer, { FooterListItem } from '@/components/design/footer'
+import useLocalStorage from '@/lib/useLocalStorage'
+
+type NotesFilter = 'my' | 'all'
 
 const NotePage: NextPage = () => {
   const { data: session } = useSession()
-  const { data: notes, isLoading } = api.notes.getAll.useQuery()
+  const { data: queriedNotes, isLoading } = api.notes.getAll.useQuery()
+  const [notesFilter, setNotesFilter] = useLocalStorage<NotesFilter>(
+    'notes-notesFilter',
+    'my'
+  )
 
+  console.log({
+    session,
+    queriedNotes,
+  })
+  const userNotes = session
+    ? queriedNotes?.filter(note => note.author === session.user.name)
+    : []
+
+  const notes = notesFilter === 'all' ? queriedNotes : userNotes
   return (
     <Page>
       <Main className='flex flex-col space-y-4 p-4'>
@@ -49,7 +69,8 @@ const NotePage: NextPage = () => {
             {notes.map(note => (
               <li key={note.id}>
                 <Link className='text-cb-pink' href={`notes/${note.id}`}>
-                  {note.title} - {note.author}
+                  {note.title}
+                  {notesFilter === 'all' ? ` - ${note.author}` : ''}
                 </Link>
               </li>
             ))}
@@ -58,6 +79,48 @@ const NotePage: NextPage = () => {
           <p>no notes</p>
         )}
       </Main>
+      <Footer>
+        <FooterListItem>
+          <Link className='flex w-full justify-center py-2' href='/'>
+            <XMarkIcon className='h-6 w-6' />
+          </Link>
+        </FooterListItem>
+        {notesFilter === 'all' ? (
+          <FooterListItem
+            onClick={() => {
+              setNotesFilter('my')
+            }}
+          >
+            <UserGroupIcon className='h-6 w-6' />
+          </FooterListItem>
+        ) : (
+          <FooterListItem
+            onClick={() => {
+              setNotesFilter('all')
+            }}
+          >
+            <UserIcon className='h-6 w-6' />
+          </FooterListItem>
+        )}
+
+        {session ? (
+          <FooterListItem
+            onClick={() => {
+              signOut().catch(err => console.log(err))
+            }}
+          >
+            <ArrowLeftOnRectangleIcon className='h-6 w-6' />
+          </FooterListItem>
+        ) : (
+          <FooterListItem
+            onClick={() => {
+              signIn('discord').catch(err => console.log(err))
+            }}
+          >
+            <ArrowRightOnRectangleIcon className='h-6 w-6' />
+          </FooterListItem>
+        )}
+      </Footer>
     </Page>
   )
 }
